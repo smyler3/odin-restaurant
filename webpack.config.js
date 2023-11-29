1615015928ffc0a1ab156be2833177f41b770953
smyler3 <mylertalym01@gmail.com>
2023-11-30 00:52:08 +1100

    Add initial draft design for splash page

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     mode: 'production',
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
 };
